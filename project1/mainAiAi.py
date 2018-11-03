333bd0046dcdd4f91f0eaa3297de17ebe19439c1
AndreCNF <andrecnf@gmail.com>
2018-11-03 17:42:53 +0000

    Created a notebook to analyse time performance

--- project1/mainAiAi.py
+++ project1/mainAiAi.py
@@ -1,25 +1,29 @@
 import go
 from alphabeta_cutoff_search import alphabeta_cutoff_search
+import pstatsviewer
 
-s = []
-endGame = 0
+def mainAiAi():
+    s = []
+    endGame = 0
 
-myGame = go.Game()
-s = myGame.load_board(open('boards/test3_1.txt', 'r'))
+    myGame = go.Game()
+    s = myGame.load_board(open('boards/test3_1.txt', 'r'))
 
-while not endGame:
-    # Print board
+    while not endGame:
+        # Print board
+        # myGame.print_board(s)
+
+        # AI 1's turn
+        AiMove = alphabeta_cutoff_search(s, myGame, d=5)
+        s = myGame.result(s, AiMove)
+        endGame = myGame.terminal_test(s)
+
+        # AI 2's turn
+        AiMove = alphabeta_cutoff_search(s, myGame, d=5)
+        s = myGame.result(s, AiMove)
+        endGame = myGame.terminal_test(s)
+
+    # Print the final board
     # myGame.print_board(s)
 
-    # AI 1's turn
-    AiMove = alphabeta_cutoff_search(s, myGame, d=10)
-    s = myGame.result(s, AiMove)
-    endGame = myGame.terminal_test(s)
-
-    # AI 2's turn
-    AiMove = alphabeta_cutoff_search(s, myGame, d=10)
-    s = myGame.result(s, AiMove)
-    endGame = myGame.terminal_test(s)
-
-# Print the final board
-# myGame.print_board(s)
+mainAiAi()
